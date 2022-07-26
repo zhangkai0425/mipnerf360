5af0464f90b71d44d9191b4fa5ce6200cd804506
zhangkai0425 <1349605984@qq.com>
2022-07-26 11:39:36 +0800

    divide the model

--- model.py
+++ model.py
@@ -2,6 +2,129 @@ import torch
 import torch.nn as nn
 from torch.nn.modules.activation import Sigmoid
 
+def _kaiming_init(model):
+    """perform kaiming initialization to the model
+    Arguments:
+        model,nn.module
+
+    Returns:
+        model,nn.module
+    """
+    for module in model.modules():
+        if isinstance(module,nn.Linear):
+            nn.init.kaiming_uniform_(module.weight)
+
+class prop_net(nn.Module):
+    def __init__(self,
+                 randomized=False,
+                 num_samples=128,
+                 hidden_proposal=256,
+                 density_bias=-1,
+                 min_deg=0,
+                 max_deg=16,
+                 viewdirs_min_deg=0,
+                 viewdirs_max_deg=4,
+                 device=torch.device("cuda"),
+                 ):
+        super().__init__()
+
+        # parameters initialize
+        self.randomized = randomized
+        self.num_samples = num_samples
+        self.hidden_proposal = hidden_proposal
+        self.density_bias = density_bias
+        self.min_deg = min_deg
+        self.max_deg = max_deg
+        self.viewdirs_min_deg = viewdirs_min_deg
+        self.viewdirs_max_deg = viewdirs_max_deg
+        self.device = device
+
+        self.input_size = 0 #TODO: self.input
+
+        # proposal network: depth = 4 width = 256
+        self.proposal = nn.Sequential(
+            nn.Linear(self.input_size,self.hidden_proposal),
+            nn.ReLU(True),
+            nn.Linear(self.hidden_proposal,self.hidden_proposal),
+            nn.ReLU(True),
+            nn.Linear(self.hidden_proposal,self.hidden_proposal),
+            nn.ReLU(True),
+            nn.Linear(self.hidden_proposal,self.hidden_proposal),
+            nn.Sigmoid(),
+            nn.Linear(self.hidden_proposal,1) # output only density 
+        )
+
+        # initialize the model ang put the model to device
+        _kaiming_init(self)
+        self.to(device)
+    def forward(self,rays):
+
+
+class nerf_net(nn.Module):
+    def __init__(self,
+                 randomized=False,
+                 num_samples=128,
+                 hidden_nerf=1024,
+                 density_bias=-1,
+                 rgb_padding=0.001,
+                 resample_padding=0.01,
+                 min_deg=0,
+                 max_deg=16,
+                 viewdirs_min_deg=0,
+                 viewdirs_max_deg=4,
+                 device=torch.device("cuda"),
+                 ):
+        super().__init__()  
+
+        # parameters initialize
+        self.randomized = randomized
+        self.num_samples = num_samples
+        self.hidden_nerf = hidden_nerf
+        self.density_bias = density_bias
+        self.rgb_padding = rgb_padding
+        self.resample_padding = resample_padding
+        self.min_deg = min_deg
+        self.max_deg = max_deg
+        self.viewdirs_min_deg = viewdirs_min_deg
+        self.viewdirs_max_deg = viewdirs_max_deg
+        self.device = device
+
+        self.input_size = 0 #TODO: self.input
+        
+        # nerf network: depth = 8 width = 1024
+        self.nerf = nn.Sequential(
+            nn.Linear(self.input_size,self.hidden_nerf),
+            nn.ReLU(True),
+            nn.Linear(self.hidden_nerf,self.hidden_nerf),
+            nn.ReLU(True),
+            nn.Linear(self.hidden_nerf,self.hidden_nerf),
+            nn.ReLU(True),
+            nn.Linear(self.hidden_nerf,self.hidden_nerf),
+            nn.ReLU(True),
+            nn.Linear(self.hidden_nerf,self.hidden_nerf),
+            nn.ReLU(True),
+            nn.Linear(self.hidden_nerf,self.hidden_nerf),
+            nn.ReLU(True),
+            nn.Linear(self.hidden_nerf,self.hidden_nerf),
+            nn.ReLU(True),
+            nn.Linear(self.hidden_nerf,self.hidden_nerf),
+            nn.Sigmoid()
+        )
+        # output the final density of nerf network
+        self.final_density = nn.Sequential(
+            nn.Linear(self.hidden_nerf,1),
+            nn.Sigmoid()
+        )   
+        # output the final color of nerf network
+        self.final_color = nn.Sequential(
+            nn.Linear(self.hidden_nerf,3),
+            nn.Sigmoid()
+        )
+        # initialize the model ang put the model to device
+        _kaiming_init(self)
+        self.to(device)
+
+
 
 class mipNeRF360(nn.Module):
     def __init__(self,
@@ -110,18 +233,7 @@ class mipNeRF360(nn.Module):
     
         
 
-def _kaiming_init(model):
-    """[summary]
-    perform kaiming initialization to the model
-    Arguments:
-        model {[nn.module]}
 
-    Returns:
-        model {[nn.module]}
-    """
-    for module in model.modules():
-        if isinstance(module,nn.Linear):
-            nn.init.kaiming_uniform_(module.weight)
 
 
 # 拓展到200行，提交10个commit

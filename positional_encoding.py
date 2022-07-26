14920c5668b4ef37ce19d56e937e76ee1ab02941
zhangkai0425 <1349605984@qq.com>
2022-07-26 14:07:53 +0800

    不完备的IPE代码，还需要修改P矩阵

---
+++ positional_encoding.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+"""
+IPE的实现：
+有个很大的问题，很大很大的问题，就是说如何将Mipnerf原来简单的形式变成论文中P矩阵的形式，
+这一点没有搞懂，真的没有搞懂，很难，非常难，非常难
+"""
+class PositionalEncoding(nn.Module):
+    def __init__(self, min_deg, max_deg):
+        super(PositionalEncoding, self).__init__()
+        self.min_deg = min_deg
+        self.max_deg = max_deg
+        self.scales = nn.Parameter(torch.tensor([2 ** i for i in range(min_deg, max_deg)]), requires_grad=False)
+
+    def forward(self, x, y=None):
+        shape = list(x.shape[:-1]) + [-1]
+        x_enc = (x[..., None, :] * self.scales[:, None]).reshape(shape)
+        x_enc = torch.cat((x_enc, x_enc + 0.5 * torch.pi), -1)
+        if y is not None:
+            # IPE
+            y_enc = (y[..., None, :] * self.scales[:, None]**2).reshape(shape)
+            y_enc = torch.cat((y_enc, y_enc), -1)
+            x_ret = torch.exp(-0.5 * y_enc) * torch.sin(x_enc)
+            y_ret = torch.maximum(torch.zeros_like(y_enc), 0.5 * (1 - torch.exp(-2 * y_enc) * torch.cos(2 * x_enc)) - x_ret ** 2)
+            return x_ret, y_ret
+        else:
+            # PE
+            x_ret = torch.sin(x_enc)
+            return x_ret

b18b817393b0afcdd4a0351b001c907027e0da42
zhangkai0425 <1349605984@qq.com>
2022-07-27 07:22:01 +0000

    我滴任务完成辣！

--- positional_encoding.py
+++ positional_encoding.py
@@ -2,10 +2,8 @@ import torch
 import torch.nn as nn
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, min_deg, max_deg):
-        super(PositionalEncoding, self).__init__()
-        self.min_deg = min_deg
-        self.max_deg = max_deg
+    def __init__(self):
+        super().__init__()
         self.P = torch.tensor([[0.8506508,0,0.5257311],
                   [0.809017,0.5,0.309017],
                   [0.5257311,0.8506508,0],
@@ -27,22 +25,35 @@ class PositionalEncoding(nn.Module):
                   [-0.5,0.309017,0.809017],
                   [-0.809017,0.5,0.309017],
                   [-0.809017,0.5,-0.309017]
-                    ])
-        self.scales = nn.Parameter(torch.tensor([2 ** i for i in range(min_deg, max_deg)]), requires_grad=False)
+                    ],requires_grad=False)
+        # self.scales = nn.Parameter(torch.tensor([2 ** i for i in range(min_deg, max_deg)]), requires_grad=False)
 
-    def forward(self, x, y=None):
-        shape = list(x.shape[:-1]) + [-1]
-        x_enc = (x[..., None, :] * self.scales[:, None]).reshape(shape)
-        x_enc = torch.cat((x_enc, x_enc + 0.5 * torch.pi), -1)
-        
-        if y is not None:
+    def forward(self, mean, cov):
+        """forward function of positional encoding
+
+        Arguments:
+            mean,torch.float32,shape(batch_size,num_samples,3),mean of each num_samples of xyz(contracted)
+            cov,torch.float32,shape(batch_size,num_samples,3,3),cov of each num_samples of xyz(contracted)
+
+        Returns:
+            enc,torch.float32,shape(batch_size,num_samples,21*2,3),postional_encoding
+        """
+
+        # shape = list(x.shape[:-1]) + [-1]
+        # x_enc = (x[..., None, :] * self.scales[:, None]).reshape(shape)
+        # x_enc = torch.cat((x_enc, x_enc + 0.5 * torch.pi), -1)
+        mean_gamma = torch.matmul(self.P,mean)
+        if cov is not None:
             # IPE
-            y_enc = (y[..., None, :] * self.scales[:, None]**2).reshape(shape)
-            y_enc = torch.cat((y_enc, y_enc), -1)
-            x_ret = torch.exp(-0.5 * y_enc) * torch.sin(x_enc)
-            y_ret = torch.maximum(torch.zeros_like(y_enc), 0.5 * (1 - torch.exp(-2 * y_enc) * torch.cos(2 * x_enc)) - x_ret ** 2)
-            return x_ret, y_ret
+            A = torch.matmul(cov,self.P.T)
+            sigma = torch.sum(self.P.T * A,dim=3) #TODO: 这里维度需要再仔细注意！
+            enc_sin = torch.exp(-0.5*sigma) * torch.sin(mean_gamma)
+            enc_cos = torch.exp(-0.5*sigma) * torch.cos(mean_gamma)
+            enc = torch.cat((enc_sin,enc_cos),-1)
+            return enc
+
         else:
             # PE
-            x_ret = torch.sin(x_enc)
-            return x_ret
+            enc = torch.cat((torch.sin(mean_gamma),torch.cos(mean_gamma)),-1)
+            return enc
+            

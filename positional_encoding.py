794355a8d2a9c6973772b688ada1df747c737c13
zhangkai0425 <1349605984@qq.com>
2022-07-27 06:07:24 +0000

    增加了P矩阵

--- positional_encoding.py
+++ positional_encoding.py
@@ -1,22 +1,40 @@
 import torch
 import torch.nn as nn
 
-"""
-IPE的实现：
-有个很大的问题，很大很大的问题，就是说如何将Mipnerf原来简单的形式变成论文中P矩阵的形式，
-这一点没有搞懂，真的没有搞懂，很难，非常难，非常难
-"""
 class PositionalEncoding(nn.Module):
     def __init__(self, min_deg, max_deg):
         super(PositionalEncoding, self).__init__()
         self.min_deg = min_deg
         self.max_deg = max_deg
+        self.P = torch.tensor([[0.8506508,0,0.5257311],
+                  [0.809017,0.5,0.309017],
+                  [0.5257311,0.8506508,0],
+                  [1,0,0],
+                  [0.809017,0.5,-0.309017],
+                  [0.8506508,0,-0.5257311],
+                  [0.309017,0.809017,-0.5],
+                  [0,0.5257311,-0.8506508],
+                  [0.5,0.309017,-0.809017],
+                  [0,1,0],
+                  [-0.5257311,0.8506508,0],
+                  [-0.309017,0.809017,-0.5],
+                  [0,0.5257311,0.8506508],
+                  [-0.309017,0.809017,0.5],
+                  [0.309017,0.809017,0.5],
+                  [0.5,0.309017,0.809017],
+                  [0.5,-0.309017,0.809017],
+                  [0,0,1],
+                  [-0.5,0.309017,0.809017],
+                  [-0.809017,0.5,0.309017],
+                  [-0.809017,0.5,-0.309017]
+                    ])
         self.scales = nn.Parameter(torch.tensor([2 ** i for i in range(min_deg, max_deg)]), requires_grad=False)
 
     def forward(self, x, y=None):
         shape = list(x.shape[:-1]) + [-1]
         x_enc = (x[..., None, :] * self.scales[:, None]).reshape(shape)
         x_enc = torch.cat((x_enc, x_enc + 0.5 * torch.pi), -1)
+        
         if y is not None:
             # IPE
             y_enc = (y[..., None, :] * self.scales[:, None]**2).reshape(shape)

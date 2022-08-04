2189757674b439c4229c6b6d79a58e91f1838e0b
zhangkai0425 <1349605984@qq.com>
2022-08-04 10:26:02 +0000

    fix bounds

--- distillation.py
+++ distillation.py
@@ -9,7 +9,7 @@ def bounds(t_vals_fine,fine_weights,t_vals_coarse):
 
     Arguments:
         t_vals_fine:torch.tensor(float32), [batch_size, num_samples+1],t_vals from the nerf_net.
-        coarse_weights:torch.tensor(float32), [batch_size, num_samples],fine weights from the nerf_net
+        fine_weights:torch.tensor(float32), [batch_size, num_samples],fine weights from the nerf_net
         T:torch.tensor(float32), [batch_size, num_samples+1],t_vals from the prop_net.
 
     Returns:
@@ -20,9 +20,17 @@ def bounds(t_vals_fine,fine_weights,t_vals_coarse):
     t1 = t_vals_fine[...,1:]
     T0 = t_vals_coarse[...,:-1]
     T1 = t_vals_coarse[...,1:]
+    fine_weights = fine_weights.detach()
+    # fine_weights and coarse weights are all 128 samples,the same shape
+    B = torch.zeros_like(fine_weights)
+    # use for loop,only 128 times,so don't worry the cost of time
+    for i in range(fine_weights.shape[-1]):
+        L,R = T0[...,i],T1[...,i]
+        B[...,i] = torch.sum(fine_weights[~((t0>R)|(t1<L))],dim=-1)
 
+    B = B.detach()
+    return B
 
-    return 0
 def loss_prop(coarse_weights,bounds):
     """[summary]
 
@@ -36,6 +44,6 @@ def loss_prop(coarse_weights,bounds):
     return 0
 def distillation(student,teacher):
     """implementation of distillation network"""
-    
+
     return True
     

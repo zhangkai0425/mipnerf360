d5413e91a89696471de001950359e456386823a9
zhankai0425 <1349605984@qq.com>
2022-08-05 08:04:00 +0000

    add funcs

--- utils.py
+++ utils.py
@@ -7,3 +7,16 @@ def normalize(x):
 
 def convolve2d(z, f):
   return signal.convolve2d(z, f, mode='same')
+
+def to_float(img):
+    if len(img.shape) >= 3:
+        return np.array([to_float(i) for i in img])
+    else:
+        return (img / 255.).astype(np.float32)
+
+def to8b(img):
+    if len(img.shape) >= 3:
+        return np.array([to8b(i) for i in img])
+    else:
+        return (255 * np.clip(np.nan_to_num(img), 0, 1)).astype(np.uint8)
+

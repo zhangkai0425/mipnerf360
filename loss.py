c6301f7cd5ae6288ee98b09770c0b650eb86bdd2
zhankai0425 <1349605984@qq.com>
2022-08-05 06:14:17 +0000

    fix loss.py

--- loss.py
+++ loss.py
@@ -12,7 +12,7 @@ def Loss_prop(t,w,t_hat,w_hat):
         w_hat:torch.tensor(float32), [batch_size, num_samples],coarse weights from the prop_net
 
     Returns:
-        loss:torch.tensor(float32),loss of the proposal
+        loss:torch.tensor(float32),loss of the proposal net 
     """
     Bounds = bounds(t_vals_fine=t,fine_weights=w,t_vals_coarse=t_hat)
     loss = loss_prop(coarse_weights=w_hat,bounds=Bounds)

c9b95655b128ab5fd7f5345febb2ca2de8b33a73
zhankai0425 <1349605984@qq.com>
2022-08-05 07:23:39 +0000

    add Loss_nerf func

--- loss.py
+++ loss.py
@@ -19,6 +19,20 @@ def Loss_prop(t,w,t_hat,w_hat):
 
     return loss
 
-def Loss_nerf():
-    return 0
+def Loss_nerf(input,target):
+    """compute the reconstrucn
+
+    Arguments:
+        input:torch.tensor(float32), [batch_size, 3],input rgb color of batch rays.
+        target:torch.tensor(float32), [batch_size, 3],target rgb color of batch rays.
+
+    Returns:
+        mse_loss:torch.tensor(float32),mse_loss
+        psnr:torch.tensor(float32),psnr
+    """
+    batch_size = input.shape[0]
+    mse_loss = ((input[...,:3] - target[..., :3]) ** 2).sum() / batch_size
+    psnr = -10.0 * torch.log10(mse_loss)
+
+    return mse_loss,psnr
     

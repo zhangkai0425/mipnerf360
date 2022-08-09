723efb009ab400dd2e329910abf8af243ecc9ace
zhankai0425 <1349605984@qq.com>
2022-08-09 08:51:48 +0000

    finish

--- regularization.py
+++ regularization.py
@@ -1,10 +1,4 @@
 import torch
-from para import g
-
-def t_to_s(t_vals,near,far):
-    """transform t to s:using the formula in the paper"""
-    s_vals = (g(t_vals) - g(near)) / (g(far) - g(near))
-    return s_vals
 
 def loss_dist(s_vals,weights):
     """compute the loss_dist according to the paper 
@@ -16,12 +10,10 @@ def loss_dist(s_vals,weights):
     Returns:
         loss_dist:torch.tensor(float32),loss_dist according to the paper  
     """
-    batch_size = weights.shape[0]
     loss_dist = 0
     for i in range(weights.shape[-1]):
         for j in range(weights.shape[-1]):
             loss_dist += torch.sum(weights[...,i] * weights[...,j] * torch.abs((s_vals[...,i] + s_vals[...,i+1])/2-(s_vals[...,j] + s_vals[...,j+1])/2))
     loss_dist += 1/3 * torch.sum(weights ** 2 * (s_vals[...,1:]-s_vals[...,:-1]))
-    loss_dist /= batch_size
 
     return loss_dist

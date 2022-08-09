303a5289e25e01bbbf949b1c7e7b7f7418852fcf
zhankai0425 <1349605984@qq.com>
2022-08-09 10:23:34 +0000

    fix some problems

--- demo/demo_360.sh
+++ demo/demo_360.sh
@@ -1,8 +1,8 @@
 # scripts for nerf_360 data
 
 # train the model
-python train.py --log_dir record/garden --dataset_name 360 --scene garden
+python train.py --log_dir record/garden --dataset_name nerf_360 --scene garden
 # visualize the model
-python video.py --log_dir record/garden --dataset_name llff --scene garden --model_weight_path record/garden/model_10000.pt
+python video.py --log_dir record/garden --dataset_name nerf_360 --scene garden --model_weight_path record/garden/model_10000.pt
 # test the model on the evaluation view directions
-python test.py --log_dir record/garden --dataset_name llff --scene garden --model_weight_path record/garden/model_10000.pt
+python test.py --log_dir record/garden --dataset_name nerf_360 --scene garden --model_weight_path record/garden/model_10000.pt
